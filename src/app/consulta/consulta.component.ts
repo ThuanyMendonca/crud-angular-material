10d5858bf5550d128dafe7a975a1ff0045c27d24
Thuany Mendonca <thuany1997@hotmail.com>
2025-05-21 00:16:00 -0300

    Tela de consulta e pesquisa clientes

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { ClienteService } from '../shared/services/cliente/cliente.service';
+import { Cliente } from '../shared/models/cliente';
+
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [MatInputModule, MatCardModule, FlexLayoutModule, MatIconModule, FormsModule, MatTableModule, MatButtonModule],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit{
+
+  nomeBusca: string = "";
+  listaClientes: Cliente[] = [];
+  colunasTable: string[] = ['id', 'nome', 'cpf', 'dataNascimento', 'email'];
+
+  constructor(private clienteService: ClienteService) { }
+
+  ngOnInit(){
+    this.listaClientes = this.clienteService.pesquisarClientes("");
+    console.log(this.listaClientes);
+  }
+
+  pesquisar(){
+    this.listaClientes = this.clienteService.pesquisarClientes(this.nomeBusca);
+  }
 
 }
